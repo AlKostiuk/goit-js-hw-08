ca0bf2913c54628f33d9e4bad7b9e2a61aa81c11
AlKostiuk <Kostiuk.A96@gmail.com>
2023-04-08 21:40:35 +0300

    add library simple light box

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,8 @@
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from "./gallery-items.js";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 // Change code below this line
 
 // console.log(galleryItems);
@@ -8,8 +12,8 @@ function getGalleryMarkup(items) {
     .map((item) => {
       return `<li class="gallery__item">
       <a class="gallery__link" href="${item.original}">
-         <img class="gallery__image" 
-         src="${item.preview}" 
+         <img class="gallery__image"
+         src="${item.preview}"
          alt="${item.description}" />
       </a>
    </li>`;
