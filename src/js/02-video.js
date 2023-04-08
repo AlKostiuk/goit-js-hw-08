788356fbab5809a5d737ee780ad38be844b09c82
AlKostiuk <Kostiuk.A96@gmail.com>
2023-04-08 22:33:17 +0300

    add time of reload

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoFrame = document.querySelector('iframe');
+const playerVimeo = new Vimeo(videoFrame);
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  playerVimeo.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+const throttleTimeUpdate = throttle(handleTimeUpdate, 1000);
+playerVimeo.on('timeupdate', throttleTimeUpdate);
+
+function handleTimeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
